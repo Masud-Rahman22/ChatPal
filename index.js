c14c32e65e5b85c67e52e6b2c43b655818164c7c
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-08 15:22:55 +0600

    initial setupis done

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT,()=> console.log(`server on port ${PORT}`))
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
 
-app.use()gi
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Project is running properly')
+})
+
+app.listen(port, () => {
+    console.log(`Project is running properly on port ${port}`);
+})
