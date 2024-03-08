4ce4c7ac74096e8a5abfb9b43e3d8a097e3ccf3c
Masud_Rahman22 <masudrhmn6560@gmail.com>
2024-03-08 18:05:55 +0600

    tested socket for real time data fetching

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http');
 const expressServer = http.createServer(app);
 const cors = require('cors');
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
 const io = new Server(expressServer);
 const port = process.env.PORT || 5000;
 
@@ -11,10 +11,18 @@ app.use(cors())
 app.use(express.json())
 
 
+io.on('connection', (socket) => {
+    console.log("user is connected")
 
+    setInterval(() => {
+        let d = new Date()
+        let t = d.getTime()
+        socket.send(t)
+    }, 10)
+})
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname+"/index.html")
+    res.sendFile(__dirname + "/index.html")
 })
 
 expressServer.listen(port, () => {
